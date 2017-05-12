856beab4fdb9f680620ad0a1b76b0ef19eba9257
784277265 <784277265@qq.com>
2017-05-12 22:37:27 +0800

    代码format

--- springboot/src/main/java/com/office/springboot/user/controller/UserController.java
+++ springboot/src/main/java/com/office/springboot/user/controller/UserController.java
@@ -12,10 +12,9 @@ import com.office.springboot.user.service.UserService;
 
 @RestController
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
 
 	@RequestMapping("/")
 	public String home() {
@@ -26,18 +25,18 @@ public class UserController {
 	public String index(@PathVariable String name) {
 		return "Hello " + name + " !";
 	}
-	
+
 	@RequestMapping("/app/user/getUserInfo")
-	public UserDTO getUserInfo(@ModelAttribute UserDTO user){
-		UserDTO result=userService.getUserInfo(user);
+	public UserDTO getUserInfo(@ModelAttribute UserDTO user) {
+		UserDTO result = userService.getUserInfo(user);
 		result.setSuccessForm("操作成功！");
 		return result;
 	}
-	
+
 	@RequestMapping("/app/user/insertUser")
-	public BaseObjectForm insertUser(@ModelAttribute UserDTO user){
-		BaseObjectForm result=new BaseObjectForm();
-		String userId=userService.insertUserWithBackId(user);
+	public BaseObjectForm insertUser(@ModelAttribute UserDTO user) {
+		BaseObjectForm result = new BaseObjectForm();
+		String userId = userService.insertUserWithBackId(user);
 		result.setData(userId);
 		result.setSuccessForm("新增成功！");
 		return result;
