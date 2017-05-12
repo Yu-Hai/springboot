d156d83876a380a46877df22a017c70e167cc3cc
Yu-Hai <784277265@qq.com>
2017-05-12 18:04:03 +0800

    提交代码

--- springboot/src/main/java/com/office/springboot/Application.java
+++ springboot/src/main/java/com/office/springboot/Application.java
@@ -1,6 +1,5 @@
 package com.office.springboot;
 
-
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
@@ -22,43 +21,40 @@ import com.office.springboot.common.util.PropertiesUtil;
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan
-@MapperScan({"com.office.springboot.common.mapper","com.office.springboot.*.mapper"})
+@MapperScan({ "com.office.springboot.common.mapper", "com.office.springboot.*.mapper" })
 public class Application {
 
-	private static Logger logger=LoggerFactory.getLogger(Application.class);  
-    
+	private static Logger logger = LoggerFactory.getLogger(Application.class);
 
-	
-    @Bean
-    @ConfigurationProperties(prefix="spring.datasource")
-    public DataSource dataSource() {
-    	DataSource dataSource = new DataSource();
-    	dataSource.setUrl(PropertiesUtil.getProperty("spring.datasource.url"));
-    	dataSource.setUsername(PropertiesUtil.getProperty("spring.datasource.username"));
-    	dataSource.setPassword(PropertiesUtil.getProperty("spring.datasource.password"));
-    	dataSource.setDriverClassName(PropertiesUtil.getProperty("spring.datasource.driver-class-name"));
-        return dataSource;
-    }
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource() {
+		DataSource dataSource = new DataSource();
+		dataSource.setUrl(PropertiesUtil.getProperty("spring.datasource.url"));
+		dataSource.setUsername(PropertiesUtil.getProperty("spring.datasource.username"));
+		dataSource.setPassword(PropertiesUtil.getProperty("spring.datasource.password"));
+		dataSource.setDriverClassName(PropertiesUtil.getProperty("spring.datasource.driver-class-name"));
+		return dataSource;
+	}
 
-    @Bean
-    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource());
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/com/office/springboot/sqlmap/*.xml"));
-        return sqlSessionFactoryBean.getObject();
-    }
-    
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
+	@Bean
+	public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource());
+		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		sqlSessionFactoryBean
+				.setMapperLocations(resolver.getResources("classpath:/com/office/springboot/sqlmap/*.xml"));
+		return sqlSessionFactoryBean.getObject();
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		logger.trace("Start Success");
+		logger.trace("Start Success ！");
 	}
-		
-	
-	
+
 }
